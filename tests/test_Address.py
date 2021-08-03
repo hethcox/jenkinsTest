0415c8ea8e83d55144db4f2d24c1d4b1eec0d1c7
John Hethcox <jhethcox@msg.us.com>
2021-08-02 21:28:47 -0400

    Re-enable tests

--- tests/test_Address.py
+++ tests/test_Address.py
@@ -1,15 +1,15 @@
-# from backend.classes.Address import Address
-# from backend.classes.Person import Person
-#
-# def test_Address():
-#     a1 = Address("17 W Nightingale", "Apopka", "FL", 32712)
-#     assert a1.street == '17 W Nightingale'
-#     assert a1.city == 'Apopka'
-#     assert a1.state == 'FL'
-#     assert a1.zip == 32712
-#
-# def test_inhabitants():
-#     a1 = Address("17 W Nightingale", "Apopka", "FL", 32712)
-#     p1 = Person("John", 36)
-#     a1.addInhabitant(p1)
-#     assert len(a1.inhabitants) == 1
+from backend.classes.Address import Address
+from backend.classes.Person import Person
+
+def test_Address():
+    a1 = Address("17 W Nightingale", "Apopka", "FL", 32712)
+    assert a1.street == '17 W Nightingale'
+    assert a1.city == 'Apopka'
+    assert a1.state == 'FL'
+    assert a1.zip == 32712
+
+def test_inhabitants():
+    a1 = Address("17 W Nightingale", "Apopka", "FL", 32712)
+    p1 = Person("John", 36)
+    a1.addInhabitant(p1)
+    assert len(a1.inhabitants) == 1
